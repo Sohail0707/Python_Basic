bb35305771523b86bb01abfcf4c2dad0aea1c7ea
Sohail <sohail2001rana@gmail.com>
2023-03-25 13:26:25 +0530

    INITIAL COMMIT

---
+++ main.py
@@ -0,0 +1,16 @@
+# PRINTING HELLO WORLD
+print("hello world")
+
+# MAKING VARTIABLES
+age = 21
+is_adult = True
+
+# TAKING INPUT
+name = input("What is your name?\n")
+
+print ("Hello " + name )
+
+# CHANGING DATA TYPE 
+age = int(input("What is your age ? \n"))
+
+print("You are "+str(age) +" years old")
